ac2293bd3d9700f867c8b1e1be1dda688e646019
Nicoleta Ursu <nicoletaursu1234@gmail.com>
2021-02-07 16:04:07 +0200

    Http request to download images

--- src/images.service.ts
+++ src/images.service.ts
@@ -1,36 +1,64 @@
+import net from "net";
 import fs from "fs";
-import axios from "axios";
+import http from "http";
 
 export const getImages = async (data): Promise<void> => {
   let imgs = new Set<string>();
   const reg = /(?:(?:https?)+\:\/\/+[a-zA-Z0-9\/\._-]{1,})+(?:(?:jpe?g|png|gif))/gims;
-
+  
   const allMatches = data.match(reg);
-  console.log('all matches:', allMatches);
 
   for (const match of allMatches) {
     if (match) {
       imgs.add(match);
-      await urlToImage(match);
-    };
-  };
-
+      await download(match);
+    }
+  }
 };
 
-export const urlToImage = async (url): Promise<void> => {
+export const download = async (url): Promise<void> => {
   const reg = /.*\.(png|jpe?g|gif)/;
   const format = url.match(reg);
-  const date = Date.now().toString();
+  const date = Date.now().toString() + new Date().getMilliseconds();
 
-  const { data } = await axios({
-    method: "get",
-    url,
-    responseType: "arraybuffer",
-  });
+  await http.get(url, (res) => {
+    res.pipe(fs.createWriteStream(`src/imgs/${date}.${format[1]}`));
+  })
+  
+//  const urlSliced = url.slice(17);
 
-  let img = await Buffer.from(data, "base64");
-  console.log(url);
-  await fs.writeFile(`src/imgs/${date}.${format[1]}`, img, (err) => {
-    err && console.error(err);
-  });
+  // const port = 80;
+  // const address = "81.180.73.230";
+
+  // const socket: net.Socket = new net.Socket();
+  // socket
+  //   .connect(port, address, () => {
+  //     console.log(`Connected to socket ${address}:${port}`);
+  //   })
+  //   .setMaxListeners(15);
+
+  // let data = [];
+  // socket.write(`GET ${urlSliced} HTTP/1.1
+  // Host: mib.utm.md
+  // Content-Type: application/octet-stream
+
+  // `);
+
+  // await socket
+  //   .on("data", (res) => {
+  //     data.push(res as never)
+  //   })
+  //   .on('error', (...all) => {console.warn(...all)})
+  //   .on("end", async () => {
+  //     let body = Buffer.concat(data);
+
+  //     await fs.writeFile(`src/imgs/${date}.${format[1]}`, body.toString(), (err) => {
+  //       err && console.error(err);
+  //     });
+
+  //   });
+
+  // socket.on("close", () => {
+  //   console.log("next");
+  // });
 };
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,21 @@
 import net from "net";
 
-<<<<<<< HEAD
-import { getImages } from "./images.service";
+import { getImages, download } from "./images.service";
 
 const port = 80;
 const address = "81.180.73.230";
 let data = "";
-=======
-import { getImages } from './images.service';
 
-const port = 80;
-const address = "81.180.73.230";
->>>>>>> d8eab94506de46385596e1566ae2fd25dd26a262
+const mainSocket = async () => {
+  const socket: net.Socket = new net.Socket();
 
-const socket: net.Socket = new net.Socket();
+  socket
+    .connect(port, address, () => {
+      console.log(`Connected to socket ${address}:${port}`);
+    })
+    .setMaxListeners(15);
 
-socket.connect(port, address, () => {
-  console.log(`Connected to socket ${address}:${port}`);
-});
-
-socket.write(`GET / HTTP/1.0
-<<<<<<< HEAD
+  socket.write(`GET / HTTP/1.1
 Host: me.utm.md
 Connection: Keep-Alive
 Content-Type: text/html; charset=UTF-8
@@ -30,27 +25,19 @@ Access-Control-Allow-Headers: Content-Type, x-requested-with
 
 `);
 
-socket.on("data", async (res) => {
-  data += await res;
-}).on("end", async () => {
-  await getImages(data);
-});
+  socket
+    .on("data", async (res) => {
+      data += await res;
+    })
+    .on("end", async () => {
+      console.log("end event");
+    });
 
-socket.on("close", () => {
-  console.log("Closing connection");
-=======
-Host: me.utm.md\r\n
-Connection: Keep-Alive\r\n
-`);
+  socket.on("close", async () => {
+    await getImages(data);
+    console.log("Closing connection");
+  });
+};
 
-socket.on("data", async (res) => {
-  let data = "";
-  
-  data += await res;
-  getImages(data);
-});
+mainSocket();
 
-socket.on("close", () => {
-  console.log('Closing connection');
->>>>>>> d8eab94506de46385596e1566ae2fd25dd26a262
-});
