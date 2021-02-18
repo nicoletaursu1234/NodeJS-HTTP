7105605269fb2c055ec4d21380d99b182d6d8b1d
Nicoleta Ursu <nicoletaursu1234@gmail.com>
2021-02-18 10:11:24 +0200

    First init

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
 import cluster from "cluster";
 
-import getHTML from "./getHTML";
-import getThreadData from "./thread";
-import deleteImages from "./deleteImages";
-
 if (cluster.isMaster) {
-  deleteImages();
 
-  getHTML("utm.md", { isHttps: true });
 } else {
-  (async () => {
-    await getThreadData();
-  })();
+ 
 }
